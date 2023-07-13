205354c8adb532d94b299929765ca0cf2f69cd91
Bachar Daowd <bachar.daowd@gmail.com>
2023-07-13 17:36:04 +0200

    Build End to End Testcase in Framework Standards -2

--- src/test/java/features/placeValidations.feature
+++ src/test/java/features/placeValidations.feature
@@ -2,10 +2,11 @@ Feature: Validating Place API's
 
   Scenario Outline: Verify if place is being successfully added using AddPlaceAPI
     Given Add Place Payload with "<name>" "<language>" "<address>"
-    When user calls "AddPlaceAPI" with Post http request
+    When user calls "AddPlaceAPI" with "Post" http request
     Then the API call is success with status code 200
     And "status" in response body is "OK"
     And "scope" in response body is "APP"
+    And verify place_Id created maps to "<name>" using "getPlaceAPI"
 
     Examples:
       | name    | language | address            |
--- src/test/java/stepDefinations/StepDefination.java
+++ src/test/java/stepDefinations/StepDefination.java
@@ -1,7 +1,5 @@
 package stepDefinations;
-
 import io.cucumber.java.en.*;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -24,19 +22,29 @@ public class StepDefination extends Utils {
     RequestSpecification resp;
     ResponseSpecification resspec;
     Response response;
-
+    String place_id;
+    JsonPath js;
     TestDataBuild data = new TestDataBuild();
+
     @Given("Add Place Payload with {string} {string} {string}")
     public void add_place_payload_with(String name, String language, String address) throws IOException {
         resp = given().spec(requestSpecification()).body(data.addPlacePayload(name, language, address));
     }
 
-    @When("user calls {string} with Post http request")
-    public void user_calls_with_post_http_request(String resource) {
+    @When("user calls {string} with {string} http request")
+    public void user_calls_with_http_request(String resource, String method) {
+        //constructor will be called with value of resource which you pass
         APIResources resourceAPI = APIResources.valueOf(resource);
         resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-        response = resp.when().post(resourceAPI.getResource()).then().spec(resspec).extract().response();
+        if (method.equalsIgnoreCase("POST"))
+            response = resp.when().post(resourceAPI.getResource());
+        else if (method.equalsIgnoreCase("GET"))
+            response = resp.when().get(resourceAPI.getResource());
+        else if (method.equalsIgnoreCase("DELETE"))
+            response = resp.when().delete(resourceAPI.getResource());
+        response.then().spec(resspec).extract().response();
     }
+
     @Then("the API call is success with status code {int}")
     public void the_api_call_is_success_with_status_code(Integer int1) {
         assertEquals(response.statusCode(), 200);
@@ -44,8 +52,16 @@ public class StepDefination extends Utils {
 
     @Then("{string} in response body is {string}")
     public void in_response_body_is(String key, String value) {
-       String res = response.asString();
-        JsonPath js = new JsonPath(res);
-        assertEquals(js.get(key).toString(), value);
+        assertEquals(getJsonPath(response, key), value);
+    }
+
+    @And("verify place_Id created maps to {string} using {string}")
+    public void verifyPlace_IdCreatedMapsToUsing(String name, String resource) throws IOException {
+        APIResources resourceAPI = APIResources.valueOf(resource);
+        place_id = getJsonPath(response, "place_id");
+        resp = given().spec(requestSpecification()).queryParam("place_id", place_id);
+        user_calls_with_http_request(resource, "GET");
+        String getName = getJsonPath(response, "name");
+        assertEquals(name, getName);
     }
 }
