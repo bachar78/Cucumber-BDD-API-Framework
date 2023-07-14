1cb5f0421e94f44b6dd6e4f9a36eb32e363c6384
Bachar Daowd <bachar.daowd@gmail.com>
2023-07-14 12:52:33 +0200

    Create Html reporting

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/features/placeValidations.feature"},
+        plugin = "json:target/jsonReports/cucumber-report.json",
         glue = "stepDefinations"
 //        ,tags = "@DeletePlace"
 )
 public class TestRunner {
 }
+//mvn test -Dcucumber.Options="--tags @DeletePlace" running from mvn
